6066c1a8221c9e74d1bd74e5b9c4549ec8e12555
Yoshimatsuo <matsukawa.y.ac@gmail.com>
2017-10-05 19:16:39 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+
+  ################################
+  #### devise向け
+  ################################
+
   devise_for :users
+
+  ################################
+  #### user向け
+  ################################
+
+  get  'home' => 'home#index'
+
   resources :views
   resources :supported_points
   resources :projects
-  get  'home' => 'home#index'
+
 end
