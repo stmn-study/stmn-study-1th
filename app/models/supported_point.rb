389a71812bef129ccb3a777c854d373befeabb9d
Yoshimatsuo <matsukawa.y.ac@gmail.com>
2017-10-18 20:37:13 +0900

    いらないもの消去

--- app/models/supported_point.rb
+++ app/models/supported_point.rb
@@ -15,22 +15,4 @@ class SupportedPoint < ApplicationRecord
   belongs_to :project
 
   validates :user, :project, :point, presence: true
-
-  class << self
-    def factory(user, project, points)
-      supported_point = SupportedPoint.new do |p|
-        p.user = user
-        p.project = project
-        p.point = points
-      end
-
-      supported_point
-    end
-
-    def factory!(*args)
-      supported_point = factory(*args)
-      supported_point.save!
-      supported_point
-    end
-  end
 end
